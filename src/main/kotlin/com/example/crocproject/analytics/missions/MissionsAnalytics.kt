1740fd21ca1dbc7fe79416a55e651e2b8d25cf72
seregatheone <sergey20030707@gmail.com>
2022-05-12 08:49:53 +0300

    reworked mission analytics

--- src/main/kotlin/com/example/crocproject/analytics/missions/MissionsAnalytics.kt
+++ src/main/kotlin/com/example/crocproject/analytics/missions/MissionsAnalytics.kt
@@ -13,24 +13,23 @@ class MissionsAnalytics(private val requestMission: RequestMission) {
         var totalTicketsAtEnd = 0.0
 
         requestMission.missions.forEach { mission ->
-            val busters = mission.multBusters.map { it.toDouble() }
-                .associateBy { "multBusters" } + mission.percentBusters.map { it.toDouble() / 100.0 }
-                .associateBy { "percentBusters" } + mission.fixed.map { it.toDouble() / mission.missionIncome }
-                .associateBy { "fixed" }
-            val allBusters = busters.toSortedMap()
+            val busters = mission.multBusters.map { it.toDouble() }.associateWith { "multBusters" }.toList() +
+                    mission.percentBusters.map { it.toDouble() / 100.0 }.associateWith { "percentBusters" }.toList() +
+                    mission.fixed.map { it.toDouble() / mission.missionIncome }.associateWith { "fixed" }.toList()
+            val allBusters = busters.sortedBy { it.first }
             val missionIncome = mission.missionIncome
             val missionBudget = mission.budget
             var spent = 0.0
-            var disc = zipListsIntoMap(allBusters.toList(), List(allBusters.size) { 0.0 })
+            var disc = zipListsIntoMap(allBusters, List(allBusters.size) { 0.0 })
             while (spent < missionBudget) {
                 val prevDisc = disc
                 val bustersRatio = disc.keys.toMutableList()
                 val ticketCount = disc.values.toMutableList()
                 for (i in bustersRatio.indices) {
-                    if (spent + bustersRatio[i].second * missionIncome <= missionBudget) {
+                    if (spent + bustersRatio[i].first * missionIncome <= missionBudget) {
                         ticketCount[i] += 1.0
                         spent = missionSpent(
-                            map = zipListsIntoMap(bustersRatio.map { it.second }, ticketCount),
+                            map = zipListsIntoMap(bustersRatio.map { it.first }, ticketCount),
                             missionReward = missionIncome
                         )
                     }
@@ -46,10 +45,10 @@ class MissionsAnalytics(private val requestMission: RequestMission) {
             totalTicketsAtEnd += sum(res.values.toList())
 
             val fixedBusters = mutableListOf<FixedBusters>()
-            res.filter { it.key.first == "fixed" }.forEach {
+            res.filter { it.key.second == "fixed" }.forEach {
                 fixedBusters.add(
                     FixedBusters(
-                        bonus = it.key.second.toInt(),
+                        bonus = (it.key.first * missionIncome).toInt(),
                         tickets = it.value.toInt()
                     )
                 )
@@ -57,10 +56,10 @@ class MissionsAnalytics(private val requestMission: RequestMission) {
             }
 
             val multBusters = mutableListOf<MultBusters>()
-            res.filter { it.key.first == "multBusters" }.forEach {
+            res.filter { it.key.second == "multBusters" }.forEach {
                 multBusters.add(
                     MultBusters(
-                        bonus = it.key.second.toInt(),
+                        bonus = (it.key.first * missionIncome).toInt(),
                         tickets = it.value.toInt()
                     )
                 )
@@ -68,10 +67,10 @@ class MissionsAnalytics(private val requestMission: RequestMission) {
             }
 
             val percentBusters = mutableListOf<PercentBusters>()
-            res.filter { it.key.first == "percentBusters" }.forEach {
+            res.filter { it.key.second == "percentBusters" }.forEach {
                 percentBusters.add(
                     PercentBusters(
-                        bonus = it.key.second.toInt(),
+                        bonus = (it.key.first * missionIncome).toInt(),
                         tickets = it.value.toInt()
                     )
                 )
@@ -82,9 +81,9 @@ class MissionsAnalytics(private val requestMission: RequestMission) {
                 MissionResponseItem(
                     spent = spent.toInt(),
                     budget = mission.budget,
-                    fixedTickets = res.keys.toList().filter { it.first == "fixed" }.size,
-                    multBusterTickets = res.keys.toList().filter { it.first == "multBusters" }.size,
-                    percentBusterTickets = res.keys.toList().filter { it.first == "percentBusters" }.size,
+                    fixedTickets = res.keys.toList().filter { it.second == "fixed" }.size,
+                    multBusterTickets = res.keys.toList().filter { it.second == "multBusters" }.size,
+                    percentBusterTickets = res.keys.toList().filter { it.second == "percentBusters" }.size,
                     fixedBusters = fixedBusters,
                     missionName = mission.missionName,
                     tickets = res.size,
@@ -94,6 +93,7 @@ class MissionsAnalytics(private val requestMission: RequestMission) {
                 )
             )
         }
+
         return ResponseMission(
             totalSpent = totalSpent.toInt(),
             totalTickets = totalTicketsAtEnd.toInt(),
